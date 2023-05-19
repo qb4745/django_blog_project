f9b51c1908e270df377c03f448b63958fd3b0385
Jaime Vicencio <jai.vicencio@duocuc.cl>
2023-05-19 02:42:49 -0400

    agregada la capacidad de modificar profiles

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):  # Inherit from UserCreationForm+
@@ -15,3 +16,18 @@ class UserRegisterForm(UserCreationForm):  # Inherit from UserCreationForm+
             "password1",
             "password2",
         ]  # Fields we want in our form and in what order
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()  # Default required=True
+
+    class Meta:  # Meta class gives us a nested namespace for configurations
+        # and keeps the configurations in one place
+        model = User
+        fields = ["username", "email"]  # Fields we want in our form and in what order
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["image"]
